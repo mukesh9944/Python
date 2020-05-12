d50513d9a2d12698b16bc3b72affb67b392b8d97
Mukesh Kumar <mukesh9944@gmail.com>
2020-05-12 19:46:18 +0530

    Adding Django forms, models and validation

--- Django_Projects/second_project/second_app/views.py
+++ Django_Projects/second_project/second_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from second_app.models import User
+#from django.http import HttpResponse
+#from second_app.models import User
+from second_app.forms import NewUserForm
 # Create your views here.
 
 def index(request):
@@ -12,6 +13,22 @@ def help(request):
     return render(request, 'second_app/help.html', context=my_dict)
 
 def user(request):
-    users_list = User.objects.order_by('first_name')
+    form = NewUserForm()
+
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('ERROR FORM INVALID')
+
+
+    return render(request, 'second_app/users.html', {'form':form})
+
+
+
+    '''users_list = User.objects.order_by('first_name')
     user_dict = {'user_records':users_list}
-    return render(request, 'second_app/user.html', context=user_dict)
+    return render(request, 'second_app/users.html', context=user_dict)'''
