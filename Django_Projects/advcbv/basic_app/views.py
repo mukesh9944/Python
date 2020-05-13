23e79b5c1e26503e22d6fdf957798f50c25cd420
Mukesh Kumar <mukesh9944@gmail.com>
2020-05-13 21:21:22 +0530

    Creating CBV class based view and CRUD operations

---
+++ Django_Projects/advcbv/basic_app/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import (View, TemplateView,
+                                    ListView, DetailView,
+                                    CreateView, UpdateView,
+                                    DeleteView)
+from . import models
+from django.http import HttpResponse
+# Create your views here.
+
+class SchoolListView(ListView):
+    context_object_name = 'schools'
+    model = models.School
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
+class SchoolCreateView(CreateView):
+    fields = ('name', 'principal', 'location')
+    model = models.School
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name', 'principal')
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+'''
+def index(request):
+    return render(request, 'index.html')
+
+class CBView(View):
+    def get(self, request):
+        return HttpResponse('CLASS BASED VIEWS ARE COOL')'''
